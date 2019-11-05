3e7ad2d48b18f936f6503e7650e7a6f676e207e9
tinahM <46954125+tinahM@users.noreply.github.com>
2019-11-05 22:01:57 +0300

    Add files via upload

--- Homepage.php
+++ Homepage.php
@@ -1,43 +1,40 @@
 <! DOCTYPE HTML>
 <html>
 <head>
-  <link rel="stylesheet" 
-		href="Homepage.css">
+ <link rel="stylesheet" 
+		href="Homepage.css"> 
+		 <script src="myScript.js"></script> 
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		
 </head>
+
 <body>
-	<div style ="background-color:#AF7AC5 ;
-				 padding-top: 20px;
-				 padding-bottom:20px;">
-		<h1><center>DOG REGISTRATION KE</h1>
-	</div>
-
-	<div style ="text-align:center;">
-		<h1>Welcome to DOG REGISTRATION KE</h1>
-		<p><font size="6">Now, you can register your dog to the national system to account for your pet!<br>
-		   Help us keep a track of the dog population in Kenya<br>
-		   If you have an account, login below. If you have not registered your dog yet, you can do so<br>
-		   by pressing the button below</font></p>
-	</div>
-	
-	<div style ="text-align:center;
-		         margin-top: 60px;">
-		         <a href="Registerme.php">
-	<button class="button">Register</button>
-</a>
-    <a href="Login.php">
-	<button class="button">Login</button>
-</a>
-
-
-	</div>
-	
-	<footer style ="background-color:#EBDEF0;
-					margin-top: 120px;
-					padding-top: 30px;
-					padding-bottom: 30px;">
-		<h3>Talk to us<h3>
-		<p>petregistration@gmail.com<br>
-								0734566278</p>
-	</footer>
+<h1 style="color: white;">DOG REGISTRATION KE</h1>
+<br><a href="Registerme.php"><button class="button" style="vertical-align:middle"><span>Register </span></button></a>
+<a href="Login.php"><button class="button" style="vertical-align:middle"><span>Login </span></button></a>
 </body>
+
+<footer>
+  <section class="about-sec parallax-section" id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <h2><small>Who We Are</small>About<br>
+          The System</h2>
+      </div>
+      <div class="col-md-4">
+       <p><font size="4"><br>In association with the government, specifically the veterinary department, this system has been built to have an online record of all the dog in the coountry.<br>Now, you can register your dog to the national system to account for your pet!<br>
+       Help us keep a track of the dog population in Kenya<br>
+       If you have an account, login above. If you have not registered your dog yet, you can also do so</font></p>
+        
+      </div>
+    </div>
+  </div>
+</section>
+</footer>
 </html>
+
+
+
+
+
